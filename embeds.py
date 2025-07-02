97c3b3dc6595efb0d631a62496c3bb5ba86fc5c1
redxsmoke <andrewevans9560@gmail.com>
2025-07-02 15:02:33 -0400

    fix embed

--- embeds.py
+++ embeds.py
@@ -1,5 +1,5 @@
 import discord
 
-def embed_message(title: str, description: str) -> discord.Embed:
-    embed = discord.Embed(title=title, description=description)
+def embed_message(title: str, description: str, color: discord.Color = discord.Color.default()) -> discord.Embed:
+    embed = discord.Embed(title=title, description=description, color=color)
     return embed
