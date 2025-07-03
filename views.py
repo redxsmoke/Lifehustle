c3ff0a0ce184dc021de444875c74acabda34bbb5
redxsmoke <andrewevans9560@gmail.com>
2025-07-03 01:08:50 -0400

    ds

--- views.py
+++ views.py
@@ -158,3 +158,12 @@ class SellFromStashView(View):
                     "❌ Something went wrong while selling your vehicle. Please try again later.",
                     ephemeral=True
                 )
+
+
+class GroceryCategoryView(View):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class GroceryStashPaginationView(View):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
