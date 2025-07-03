300cad919b174c15e7e8a1c7d49b804d262309e7
redxsmoke <andrewevans9560@gmail.com>
2025-07-02 22:08:50 -0400

    fix

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from db_user import reset_user_finances_table
-from sql_update import remove_vehicle_by_id
+
 
 
 
@@ -274,7 +274,7 @@ async def on_ready():
         await setup_user_finances_table(globals.pool)
         await reset_user_finances_table(globals.pool)
         await rename_username_column(globals.pool)
-        await remove_vehicle_by_id(globals.pool)        
+             
 
     print(f"✅ Logged in as {bot.user} (ID: {bot.user.id})")
 
