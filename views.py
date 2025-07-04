a7533d849f49735efeda4b891e2e6ee42e5c7ea8
redxsmoke <andrewevans9560@gmail.com>
2025-07-04 13:59:10 -0400

    asd

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ui import View, Button, Select
 from discord import Interaction, Embed, Color
-from embeds import COLOR_GREEN, COLOR_RED
+from embeds import  embed_message, COLOR_GREEN, COLOR_RED
 import traceback
 from db_user import get_user, upsert_user, get_user_finances, upsert_user_finances
 import utilities
