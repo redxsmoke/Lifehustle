c7c25623be37d46881bbbaf3938826e3f946fbf1
redxsmoke <andrewevans9560@gmail.com>
2025-07-04 01:57:59 -0400

    dsa

--- embeds.py
+++ embeds.py
@@ -1,6 +1,6 @@
 import discord
 COLOR_RED = discord.Color.red()
-COLOR_GREEN = discord.color.green()
+COLOR_GREEN = discord.Color.green()
 
 def embed_message(title, description, color):
     embed = discord.Embed(title=title, description=description, color=color)
