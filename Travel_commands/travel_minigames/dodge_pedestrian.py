476b0205415ddc6fad31cb14e5480023f7f76d01
redxsmoke <andrewevans9560@gmail.com>
2025-07-22 13:31:30 -0400

    asd

--- Travel_commands/travel_minigames/dodge_pedestrian.py
+++ Travel_commands/travel_minigames/dodge_pedestrian.py
@@ -123,7 +123,7 @@ class TravelMiniGameView(View):
     def build_obstacle_scene(self, step):
         road = "🛣️"
         car = "🚗"
-        empty = ""  # no emoji
+        empty = "⬛"  # Black square for empty lane to keep alignment
 
         obstacles = self.obstacle_lanes[step]
         icons = {
@@ -137,7 +137,6 @@ class TravelMiniGameView(View):
         bottom = " ".join(car if lane == self.current_lane else empty for lane in self.lanes)
 
         return f"{top}\n{bottom}"
-
 
     # These predicament functions are no longer called except to provide messages; obstacles generated once
     async def predicament_1(self, user_lane, idx):
