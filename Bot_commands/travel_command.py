5b3d0cca821de47e4a33e04dd0dc05a4b409c117
redxsmoke <andrewevans9560@gmail.com>
2025-07-23 09:01:37 -0400

    sdaf

--- Bot_commands/travel_command.py
+++ Bot_commands/travel_command.py
@@ -497,6 +497,7 @@ async def handle_travel_with_vehicle(interaction, vehicle, method, user_travel_l
                 f"Your {vehicle.get('vehicle_type', 'vehicle')} is not at your current location — it’s currently at **{vehicle_location_name}**.\n\nWould you like to retrieve it for $200? Otherwise, you can take public transit (bus or subway) back to work to pick it up",
                 COLOR_RED
             ),
+            view=view,
             ephemeral=True
         )
         await view.wait()
